2556a540d8296c3de7bf05c7a10ae3b9c400a193
dimon-ton <saichon2namoom@gmail.com>
2022-11-12 17:03:24 +0700

    EP.5 seek at 1:94:05

--- mywebsite/company/models.py
+++ mywebsite/company/models.py
@@ -15,7 +15,8 @@ class Product(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    quantity = models.IntegerField(null=True, blank=True)
+    quantity = models.IntegerField(default=1,null=True, blank=True)
+    instock = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
